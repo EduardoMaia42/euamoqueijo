ab4140c46f55d9658a45f1ee13c6b9006bd717e2
EduardoMaia42 <maia.eduardo42@gmail.com>
2017-04-25 11:59:19 -0300

    aquivos iniciais

---
+++ logica.php
@@ -0,0 +1,7 @@
+<?php
+$titulo     = ['Dom', 'Nobre', 'Paladino'];
+$qualidade  = ['Abridor de galáxias', 'Salvador da pátria', 'O maix pica'];
+$nome       = 'Jefferson';
+
+$tituloVisualizado    = $titulo[rand(0,2)];
+$qualidadeVisualizada = $qualidade[rand(0,2)];
